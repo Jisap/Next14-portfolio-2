7a83b3bb34a849005642c6bb12135450e953d475
Jorge Matos <jisapjisap@gmail.com>
2024-08-20 19:07:21 +0200

    HeroSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/sections/Header";
+import { HeroSection } from "@/sections/Hero";
 
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,40 @@
+import memojiImage from '@/assets/images/memoji-computer.png'
+import Image from 'next/image';
+import ArrowDown from '@/assets/icons/arrow-down.svg'
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className='py-32'>
+      <div className="container">
+        <div className='flex flex-col items-center'>
+          <Image
+            src={memojiImage}
+            alt="Person peeking from behind laptop"
+            className='size-[100px]'
+          />
+          <div className='bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex'>
+            <div></div>
+            <div>Available for a new projects</div>
+          </div>
+        </div>
+      <h1>Building Exceptional User Experiences</h1>
+      </div>
+
+
+      <p>
+        I specialize in transforming designs into funcitonal, high-performing web applications. Let's discuss your next project.
+      </p>
+
+      <div>
+        <button>
+          <span>Explore My Work</span>
+          <ArrowDown />
+        </button>
+        <button>
+          <span>🖐</span>
+          Connect
+        </button>
+      </div>
+    </div>
+  )
 };
