5d0ea330ff0874f3f55f2e484921f6ed199d9228
Jorge Matos <jisapjisap@gmail.com>
2024-08-20 19:25:22 +0200

    HeroSection

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ import ArrowDown from '@/assets/icons/arrow-down.svg'
 
 export const HeroSection = () => {
   return (
-    <div className='py-32'>
+    <div className='py-32 md:py-48'>
       <div className="container">
         <div className='flex flex-col items-center'>
           <Image
@@ -17,14 +17,16 @@ export const HeroSection = () => {
             <div className='text-sm font-medium'>Available for a new projects</div>
           </div>
         </div>
-      <h1 className='font-serif text-3xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
       </div>
 
-      <p className='mt-4 text-center text-white/60'>
-        I specialize in transforming designs into funcitonal, high-performing web applications. Let's discuss your next project.
-      </p>
+      <div className='max-w-lg mx-auto'>
+        <h1 className='font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide'>Building Exceptional User Experiences</h1>
+        <p className='mt-4 text-center text-white/60 md:text-lg'>
+          I specialize in transforming designs into funcitonal, high-performing web applications. Let's discuss your next project.
+        </p>
+      </div>
 
-      <div className='flex flex-col items-center mt-8 gap-4'>
+      <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
         <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
           <span className='font-semibold'>Explore My Work</span>
           <ArrowDown className='size-4'/>
