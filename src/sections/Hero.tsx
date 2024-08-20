fccd77f5edb59a637b8e598a648fdf2f22cbcbe9
Jorge Matos <jisapjisap@gmail.com>
2024-08-20 20:13:12 +0200

    HeroSection

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,11 +2,12 @@ import memojiImage from '@/assets/images/memoji-computer.png'
 import Image from 'next/image';
 import ArrowDown from '@/assets/icons/arrow-down.svg'
 import grainImage from '@/assets/images/grain.jpg'
+import StarIcon from '@/assets/icons/star.svg'
 
 export const HeroSection = () => {
   return (
     <div className='py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip'>
-      
+
       {/* imagen de grano */}
       <div className='absolute inset-0 -z-30 opacity-5' style={{
         backgroundImage: `url(${grainImage.src})`
@@ -17,6 +18,15 @@ export const HeroSection = () => {
       <div className='size-[820px] hero-ring'></div>
       <div className='size-[1020px] hero-ring'></div>
       <div className='size-[1220px] hero-ring'></div>
+
+      {/* Stars */}
+      <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 '>
+        <div className='border border-red-500 size-[800px] animate-spin [animation-duration:30s]'>
+          <div className='border border-red-500 inline-flex animate-spin [animation-duration:5s]'>
+            <StarIcon className="size-28 text-emerald-300" />
+          </div>
+        </div>
+      </div>
 
       <div className="container">
         <div className='flex flex-col items-center'>
@@ -42,7 +52,7 @@ export const HeroSection = () => {
       <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4'>
         <button className='inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl'>
           <span className='font-semibold'>Explore My Work</span>
-          <ArrowDown className='size-4'/>
+          <ArrowDown className='size-4' />
         </button>
         <button className='inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl'>
           <span className=''>🖐</span>
