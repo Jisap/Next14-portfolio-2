28afda9e22b681869f05d1c284f1e18a6408a79d
Jorge Matos <jisapjisap@gmail.com>
2024-08-24 12:51:45 +0200

    Project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,6 +3,7 @@ import lightSaasLandingPage from "../assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import darkSaasLandingPage from '@/assets/images/dark-saas-landing-page.png';
 import Image from "next/image";
+import CheckCircleIcon from "@/assets/icons/check-circle.svg"
 
 const portfolioProjects = [
   {
@@ -47,21 +48,43 @@ export const ProjectsSection = () => {
   return (
     <div>
       <div className="container">
-        <p>Real-world Results</p>
-        <h2>Featured Projects</h2>
-        <p>See how I transformed concepts into engaging digital experiences</p>
-        <div>
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent text-center">
+            Real-world Results
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 mt-4">
+          See how I transformed concepts into engaging digital experiences
+        </p>
+        <div className="flex flex-col mt-10">
           {portfolioProjects.map((project) => (
-            <div key={project.title}>
-              <div>
-                <span>{project.company}</span>
-                <span>{project.year}</span>
+            <div 
+              key={project.title} 
+              className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] 
+              after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl
+              after:outline-white/20 p-8"
+            >
+              <div className="flex">
+                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase 
+                tracking-widest text-sm text-transparent bg-clip-text">
+                  <span>{project.company}</span>
+                  <span>&bull;</span>
+                  <span>{project.year}</span>
+                </div>
               </div>
-              <h3>{project.title}</h3>
-              <hr />
-              <ul>
+              <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
+              <hr className="border-t-2 border-white/5 mt-4"/>
+              <ul className="flex flex-col gap-4 mt-4">
                 {project.results.map((result) => (
-                  <li key={project.title}>{result.title}</li>
+                  <li key={project.title} className="flex gap-2 text-sm text-white/25">
+                    <CheckCircleIcon className="size-5"/>
+                    <span>
+                      {result.title}
+                    </span>
+                  </li>
                 ))}
               </ul>
               <a href={project.link}>
