3118f750dfb3d4eb0ec4712b48085bd8cad26c22
Jorge Matos <jisapjisap@gmail.com>
2024-08-25 21:54:05 +0200

    tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -7,6 +7,7 @@ const words = [
   "Interactive",
   "Scalable",
   "User Friendly",
+  "Resposive",
   "Maintainable",
   "Search Optimized",
   "Usable",
@@ -15,7 +16,7 @@ const words = [
 
 export const TapeSection = () => {
   return (
-    <div className='py-16'>
+    <div className='py-16 lg:py-24 overflow-x-clip'>
       <div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
         <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
           <div className='flex flex-none gap-4 py-3'>
