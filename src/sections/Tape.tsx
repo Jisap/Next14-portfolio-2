d20a446c5a171697c51ce0204b8f0aebfa948d2a
Jorge Matos <jisapjisap@gmail.com>
2024-08-25 19:52:19 +0200

    tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,29 @@
+import StarIcon from '@/assets/icons/star.svg'
+
+const words = [
+  "performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+]
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div>
+      <div>
+        {words.map((word) => (
+          <div key={word}>
+            <span>{word}</span>
+            <StarIcon />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 };
