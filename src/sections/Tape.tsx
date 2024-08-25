090d0e32047669fc596c23e52d928764577b0936
Jorge Matos <jisapjisap@gmail.com>
2024-08-25 21:49:46 +0200

    tape

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -16,14 +16,16 @@ const words = [
 export const TapeSection = () => {
   return (
     <div className='py-16'>
-      <div className='bg-gradient-to-r from-emerald-300 to-sky-400'>
-        <div className='flex gap-4'>
-          {words.map((word) => (
-            <div key={word} className='inline-flex gap-4'>
-              <span>{word}</span>
-              <StarIcon className="size-5" />
-            </div>
-          ))}
+      <div className='bg-gradient-to-r from-emerald-300 to-sky-400 overflow-x-clip -rotate-3 -mx-1'>
+        <div className='flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]'>
+          <div className='flex flex-none gap-4 py-3'>
+            {words.map((word) => (
+              <div key={word} className='inline-flex gap-4 items-center'>
+                <span className='text-gray-900 uppercase font-extrabold text-sm'>{word}</span>
+                <StarIcon className="size-5 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
