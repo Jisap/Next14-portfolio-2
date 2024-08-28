e1439dd30da70131baf1110eff7e9d42956b2456
Jorge Matos <jisapjisap@gmail.com>
2024-08-28 15:08:37 +0200

    card

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -62,12 +62,8 @@ export const ProjectsSection = () => {
           {portfolioProjects.map((project) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:pt-12 md:px-10 lg:pt-16 lg:px-20"
             >
-
-              <div className="absolute inset-0 -z-10 opacity-5" style={{
-                backgroundImage: `url(${grainImage.src})`
-              }}></div>
 
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
