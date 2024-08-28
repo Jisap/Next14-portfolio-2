c4d9737793c8768d577923e86c44ee68ffcca7bc
Jorge Matos <jisapjisap@gmail.com>
2024-08-28 15:27:26 +0200

    card

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -50,20 +50,31 @@ export const TestimonialsSection = () => {
           title="What Clients Say about me"
           description="Don't just take my word for it. See what my clients have to say about my work."
         />
-        <div>
+        <div className="mt-16 flex">
+          <div className="flex gap-8 flex-none">
           {testimonials.map((testimonial) => (
             <Card
-              key={testimonial.name} 
-            >
-              <Image
-                src={testimonial.avatar} 
-                alt={testimonial.name}
-              />
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
+              key={testimonial.name}
+              className="max-w-xs"
+              >
+              <div className="flex gap-4 items-center">
+                <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full">
+                  <Image
+                    src={testimonial.avatar}
+                    alt={testimonial.name}
+                    className="max-h-full"
+                    />
+                </div>
+                <div>
+                  <div className="font-semibold">{testimonial.name}</div>
+                  <div className="text-sm text-white/40">{testimonial.position}</div>
+                </div>
+              </div>
+
+              <p className="mt-4 text-sm">{testimonial.text}</p>
             </Card>
           ))}
+          </div>
         </div>
       </div>
     </div>
