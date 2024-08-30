a4a5c9d99e2f59306c46ae7f79aab3896c942875
Jorge Matos <jisapjisap@gmail.com>
2024-08-30 18:24:55 +0200

    3th card

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -47,30 +47,44 @@ const hobbies = [
   {
     title: 'Painting',
     emoji: '🎨',
+    left: '5%',
+    top: '5%',
   },
   {
     title: 'Photography',
     emoji: '🎞',
+    left: '50%',
+    top: '5%',
   },
   {
     title: 'Gaming',
     emoji: '🎮',
+    left: '10%',
+    top: '35%',
   },
   {
     title: 'Hiking',
     emoji: '🥾',
+    left: '35%',
+    top: '40%',
   },
   {
     title: 'Music',
     emoji: '🎷',
+    left: '70%',
+    top: '45%',
   },
   {
     title: 'Fitness',
     emoji: '👟',
+    left: '5%',
+    top: '65%',
   },
   {
     title: 'Reading',
     emoji: '📚',
+    left: '45%',
+    top: '70%',
   },
 ]
 
@@ -113,16 +127,23 @@ export const AboutSection = () => {
               itemsWrapperClassName="-translate-x-1/2"
             />
           </Card>
-          <Card className="h-[320px] p-0">
+          <Card className="h-[320px] p-0 flex flex-col">
             <CardHeader
               title="Beyond the code"
               description="Explore my interest and hobbies beyond the digital realm."
-              className="px-6 pt-6"
+              className="px-6 py-6"
             />
-            <div>
+            <div className="relative flex-1">
               {hobbies.map((hobby) => (
-                <div key={hobby.title}>
-                  <span>{hobby.title}</span>
+                <div 
+                  key={hobby.title} 
+                  className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                  style={{
+                    left: hobby.left,
+                    top: hobby.top,
+                  }}
+                >
+                  <span className="font-medium text-gray-950">{hobby.title}</span>
                   <span>{hobby.emoji}</span>
                 </div>
               ))}
