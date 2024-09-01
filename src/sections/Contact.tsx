6512a910f3d129cbef3606dcc2df3ddfcc15dff6
Jorge Matos <jisapjisap@gmail.com>
2024-09-01 13:49:49 +0200

    init contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -6,16 +6,18 @@ export const ContactSection = () => {
   return (
     <div className='py-16'>
       <div className='container'>
-        <h2>Let's create something amazing together</h2>
-        <p>
-          Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
-        </p>
-        <button>
-          <span>
-            Contact Me
-          </span>
-          <ArrowUpRightIcon />
-        </button>
+        <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center'>
+          <h2 className='font-serif text-2xl'>Let's create something amazing together</h2>
+          <p className='text-sm mt-2'>
+            Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
+          </p>
+          <button className='text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 mt-8'>
+            <span className='font-semibold'>
+              Contact Me
+            </span>
+            <ArrowUpRightIcon className="size-4" />
+          </button>
+        </div>
       </div>
     </div>
   )
