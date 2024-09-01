2d32e0e0c30910a77a77f169b92c1b4dcec4a07f
Jorge Matos <jisapjisap@gmail.com>
2024-09-01 13:51:27 +0200

    init contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,9 +4,9 @@ import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 
 export const ContactSection = () => {
   return (
-    <div className='py-16'>
+    <div className='py-16 pt-14'>
       <div className='container'>
-        <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center'>
+        <div className='bg-gradient-to-r from-emerald-300 to-sky-400 text-gray-900 py-8 px-10 rounded-3xl text-center relative'>
           <h2 className='font-serif text-2xl'>Let's create something amazing together</h2>
           <p className='text-sm mt-2'>
             Ready to bring your next project to life? Let's connect and discuss how I can help you achieve your goals.
