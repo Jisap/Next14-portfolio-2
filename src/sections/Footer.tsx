2ebf4e9ce3b046165da74197cd07c9d1e596c56b
Jorge Matos <jisapjisap@gmail.com>
2024-09-02 20:02:38 +0200

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,33 @@
+
+const footerLinks = [
+  {
+    title: 'YouTube',
+    href: '#',
+  },
+  {
+    title: 'Twitter',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'Linkedin',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div>
+          <div>&copy; 2024. All right reserved.</div>
+          <nav>
+            
+          </nav>
+        </div>
+      </div>
+    </footer>)
 };
