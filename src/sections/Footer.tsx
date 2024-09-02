5a0957e265b3a05345ba39c74dd1627f633e2c61
Jorge Matos <jisapjisap@gmail.com>
2024-09-02 20:08:47 +0200

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,4 @@
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg';
 
 const footerLinks = [
   {
@@ -22,10 +23,17 @@ export const Footer = () => {
   return (
     <footer>
       <div className="container">
-        <div>
-          <div>&copy; 2024. All right reserved.</div>
-          <nav>
-            
+        <div className='border-t border-white/15 py-6 text-sm flex flex-col items-center'>
+          <div className='text-white/40'>&copy; 2024. All right reserved.</div>
+          <nav className='flex flex-col items-center'>
+            {footerLinks.map((link) => (
+              <a href={link.href} key={link.title} className='inline-flex items-center gap-1.5'>
+                <span className='font-semibold'>
+                  {link.title}
+                </span>
+                <ArrowUpRightIcon  className="size-4"/>
+              </a>
+            ))}
           </nav>
         </div>
       </div>
